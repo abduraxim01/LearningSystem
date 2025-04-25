d71c2bf58c55eb502146ecf4bccdf363b6bdf7ca
Abduraxim <150508362+abduraxim01@users.noreply.github.com>
2025-04-25 22:23:55 +0500

    Add files via upload

---
+++ src/main/java/uz/abduraxim/LearningSystem/LearningSystemApplication.java
@@ -0,0 +1,13 @@
+package uz.abduraxim.LearningSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uz/abduraxim/LearningSystem/LearningSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.abduraxim.LearningSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
